a9f615f165d115816b0b5aaf96f8ef7e10186b9d
d0nghyun <136547988+d0nghyun@users.noreply.github.com>
2024-06-18 22:26:28 +0900

    Delete alpha directory

--- alpha/dummy/helper.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime, timedelta
-
-
-def calculate_previous_start_date(start_date, lookback_days):
-    start = datetime.strptime(str(start_date), "%Y%m%d")
-    previous_start = start - timedelta(days=lookback_days)
-    return int(previous_start.strftime("%Y%m%d"))
